c015b8d52df546a905790988b56b9400419faae8
Tống <anh.tn184040@sis.hust.edu.vn>
2020-12-07 16:43:24 +0700

    comeback the code

--- models/user.js
+++ models/user.js
@@ -1,35 +1,33 @@
-// const User = (sequelize, type) => sequelize.define('User',
-//   {
-//     id: {
-//       type: type.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     username: {
-//       type: type.STRING,
-//       allowNull: false,
-//     },
-//     email: {
-//       type: type.STRING,
-//       allowNull: false,
-//     },
-//     password: {
-//       type: type.STRING,
-//       allowNull: false,
+module.exports = (sequelize, type) => sequelize.define('user', {
+  id: {
+    type: type.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: type.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: type.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: type.STRING,
+    allowNull: false,
 
-//     },
-//     resetPasswordToken: {
-//       type: type.STRING,
-//     },
-//     resetPasswordExpires: {
-//       type: type.DATE,
-//     },
-//     state: {
-//       type: type.STRING,
-//     },
-//   }, {
-//     tableName: 'Users',
-//     timestamps: false,
-//   });
+  },
+  resetPasswordToken: {
+    type: type.STRING,
+  },
+  resetPasswordExpires: {
+    type: type.DATE,
+  },
+  state: {
+    type: type.STRING,
 
-// module.exports = { User };
+  },
+}, {
+  tableName: 'Users',
+  timestamps: false,
+});
